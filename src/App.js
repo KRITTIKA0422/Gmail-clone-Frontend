a705e6f6c02402ac672e1421d723f296bd9f0051
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-07-16 17:17:37 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import logo from './logo.svg';
 import "./App.css";
-
+import {useState} from "react";
+import { useEffect } from 'react';
 
 
 // App - component
 function App() {
-  const mobiles = [
-    {
-      model: "OnePlus 9 5G",
-      img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-      company: "Oneplus"
-    },
-    {
-      model: "Iphone 13 mini",
-      img:
-        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-      company: "Apple"
-    },
-    {
-      model: "Samsung s21 ultra",
-      img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-      company: "Samsung"
-    },
-    {
-      model: "Xiomi mi 11",
-      img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-      company: "Xiomi"
-    }
-  ];
+  const[mobiles,setMobiles]=useState({});
+  useEffect(()=>{
+    fetch(`http://localhost:4000/mobiles`,{
+      method:"GET",
+    })
+    .then((data)=>data.json())
+    .then((m)=>setMobiles (m));
+  },[]);
   
   return (
     <div className="App">
